e1a4fb2f90ab10ef4d96437fdaa12a12c6513863
burn1ng <andrew.burn1ng@gmail.com>
2017-07-21 17:43:35 +0300

    CRUD routing and mongo.db added

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,7 +1,69 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(appExpress, db) {
+
+    // CREATE route
     appExpress.post('/notes', (req, res) => {
-        // Здесь будем создавать заметку.
-        console.log(req.body)
-        res.send('Hello')
+
+        const note = { text: req.body.body, title: req.body.title };
+
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+
+        // console.log(req.body)
+        // res.send('Hello')
     });
+
+    // READ route
+    appExpress.get('/notes/:id', (req, res) => {
+
+        const id = req.params.id;
+        //MongoDB require ID not as String, but in sprecial [ObjectID] object
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    // UPDATE route (PUT = read + create logic)
+    appExpress.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
+
+    // DELETE route
+    appExpress.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
+
 };
--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const appExpress            = express();
-const port = 8000;
+const db             = require('./config/db');
+const appExpress     = express();
+const port           = 8000;
 
 appExpress.use(bodyParser.urlencoded({ extended: true }));
-require('./routes')(appExpress, {});
 
-appExpress.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+
+  require('./routes')(appExpress, database);
+
+  appExpress.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
